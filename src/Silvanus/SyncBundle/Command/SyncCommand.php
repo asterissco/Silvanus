f23982e9e87bf1d595740d2c6d870b5c157ff42f
Alejandro Cabrera <asterissco6@gmail.com>
2013-08-28 19:39:02 +0100

    Create a sync command with test

--- src/Silvanus/SyncBundle/Command/SyncCommand.php
+++ src/Silvanus/SyncBundle/Command/SyncCommand.php
@@ -40,14 +40,97 @@ class SyncCommand extends ContainerAwareCommand
 			
 			$chainEntity = $this->em->getRepository('SilvanusChainsBundle:Chain')->findOneBy(array('id'=>$syncEntity->getChainId()));
 			
-			echo '#'.$chainEntity->getId().' '.$chainEntity->getName().' '.$chainEntity->getPolicy()."\n";
+			echo '#'.$chainEntity->getId().' '.$chainEntity->getName()."\n";
 			
-			//create test chain
+			//delete and create test chain
+			exec('iptables -F silvanus_test_chain 2>&1',$trash);
+			exec('iptables -X silvanus_test_chain 2>&1',$trash);
 			exec('iptables -N silvanus_test_chain');
 			
+			//load and test rules for this chain
+			$builder 	= $this->em->getRepository('SilvanusFirewallRulesBundle:FirewallRules')->createQueryBuilder('f');
+			$query 		= $builder
+							->where('f.chain_id = :id_chain')
+							->setParameter(':id_chain',$chainEntity->getId())
+							->orderBy('f.priority','asc')
+							->getQuery();
+			
+			$firewallRulesEntities = $query->getResult();
+			
+			$errorHandle = false;
+			
+			//test loop
+			foreach($firewallRulesEntities as $firewallRulesEntity){
+				
+				$rule = "iptables -I silvanus_test_chain ".$firewallRulesEntity->getPriority()." ".$firewallRulesEntity->getRule()." 2>&1 ";
+				
+				if($chainEntity->getHost()!=''){
+				
+					$rule = str_replace('/host/',' '.$chainEntity->getHost().' ',$rule);
+				
+				}
+				
+				unset($output);
+				exec($rule,$output);
+				
+				//error found
+				if(count($output)>0){
+					echo ' [error]	';
+					$errorHandle = true;
+				//no error found	
+				}else{
+					echo ' [ok]		';
+				}
+				
+				echo $rule;
+				
+				if($errorHandle){
+					print_r($output);
+				}
+					
+				echo "\n";
+
+				
+			}
+
+			exec('iptables -F silvanus_test_chain 2>&1',$trash);
+			exec('iptables -X silvanus_test_chain 2>&1',$trash);
+
+			
+			//no errors found
+			if(!$errorHandle){
+			
+				//delete and create test chain
+				exec('iptables -F '.$chainEntity->getName().' 2>&1',$trash);
+				exec('iptables -X '.$chainEntity->getName().' 2>&1',$trash);
+				exec('iptables -N '.$chainEntity->getName());
+				
+				//set the rules to chain
+				foreach($firewallRulesEntities as $firewallRulesEntity){
+				
+					$rule = "iptables -I ".$chainEntity->getName()." ".$firewallRulesEntity->getPriority()." ".$firewallRulesEntity->getRule()." 2>&1 ";
+				
+					exec($rule);
+					
+				}
+			
+				//delete the petition
+				$this->em->remove($syncEntity);
+				
+				
+			}else{
+				
+				$syncEntity->setError(true);
+				$this->em->persist($syncEntity);
+				
+			}
+				
 		}
+		
+		$this->em->flush();
 
 	}
+
 }
 
 
